a5b2c82caf47baf65e340fd5f2c54aa9c819551e
Richard Vu <vukhai09112005@gmail.com>
2025-07-03 14:22:14 +0700

    hocState

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,9 +1,14 @@
 import React from 'react';
 class MyComponent extends React.Component {
+    state = {
+        name: 'Kai',
+        address: 'Khai Richard',
+        num: 11
+    }
     render() {
         return (
-            <div>my first component
-                {Math.random()}
+            <div>
+                My name is {this.state.name} and my nickname is {this.state.address}
             </div>
         );
     }
