b6128687540ff291419e6e401ca59eedac31b738
Richard Vu <vukhai09112005@gmail.com>
2025-07-04 15:59:24 +0700

    HocProps

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import UserInfor from './UserInfo';
+import DisplayInfor from './DisplayInfor';
 class MyComponent extends React.Component {
 
     render() {
         return (
             <div>
-
                 <UserInfor />
+                <br></br>
+                <DisplayInfor name="KhaiVu911" age="30" />
+                <hr></hr>
+                <DisplayInfor name="Kevin" age={27} />
             </div>
         );
     }
