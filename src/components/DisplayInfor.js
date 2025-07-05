b634381ab7f383398be77176af31b02628c9be7f
Richard Vu <vukhai09112005@gmail.com>
2025-07-05 12:16:37 +0700

    NestComponent

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
-class DisplayInfor extends React.Component{
-    render(){
-        const {age, name} = this.props;
+class DisplayInfor extends React.Component {
+    render() {
+        const { listUsers } = this.props;
+        console.log(listUsers)
+        // const  listUsers = this.props.listUsers;
         return (
             <div>
+                {listUsers.map((user) => {
+                    return (
+                        <div key={user.id}>
+                            <div>My name's {user.name}</div>
+                            <div>My age's {user.age}</div>
+                            <hr></hr>
+                        </div>
+                    )
+                })}
+                {/* <div>My name's {name}</div>
+                <div>My age's {age}</div>
+                <hr></hr>
                 <div>My name's {name}</div>
                 <div>My age's {age}</div>
+                <hr></hr>
+                <div>My name's {name}</div>
+                <div>My age's {age}</div> */}
             </div>
         )
     }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,15 +2,23 @@ import React from 'react';
 import UserInfor from './UserInfo';
 import DisplayInfor from './DisplayInfor';
 class MyComponent extends React.Component {
-
+    state={
+        listUsers: [
+            {id: 1, name:"Ronaldo", age:"28"},
+            {id: 2, name:"Messi", age:"27"},
+            {id: 3, name:"Neymar", age:"24"}
+        ]
+    }
+    // DRY: Don't repeat yourself 
     render() {
         return (
             <div>
                 <UserInfor />
                 <br></br>
-                <DisplayInfor name="KhaiVu911" age="30" />
-                <hr></hr>
-                <DisplayInfor name="Kevin" age={27} />
+
+                <DisplayInfor 
+                listUsers={this.state.listUsers} 
+                />
             </div>
         );
     }
