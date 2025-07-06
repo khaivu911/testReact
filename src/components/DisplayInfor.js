df497e025385c869460236e0c3d651e93c2f8919
Richard Vu <vukhai09112005@gmail.com>
2025-07-06 22:49:38 +0700

    hocSCSS

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './DisplayInfor.scss';
 
 class DisplayInfor extends React.Component {
     state = {
@@ -15,7 +16,7 @@ class DisplayInfor extends React.Component {
         const { listUsers } = this.props;
         // console.log(listUsers)
         return (
-            <div>
+            <div className='display-infor-container'>
                 <div>
                     <span onClick={() => { this.handleShowHide() }}> 
                         {this.state.isShowListUser === true ? "Hide list users:" :"Show list users:"}
@@ -27,7 +28,7 @@ class DisplayInfor extends React.Component {
                             console.log("check: ", user)
                             return (
                                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                                    <div>My name's {user.name}</div>
+                                    <div style={{color: 'purple', paddingTop: '15px'}} >My name's {user.name}</div>
                                     <div>My age's {user.age}</div>
                                     <hr></hr>
                                 </div>
