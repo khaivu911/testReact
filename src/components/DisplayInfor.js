c87b7a352174e788f9c0194182c3aada9ae465e8
Richard Vu <vukhai09112005@gmail.com>
2025-07-08 23:33:44 +0700

    hocState

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,66 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './DisplayInfor.scss';
 import logo from './../logo.svg'
-class DisplayInfor extends React.Component {
-    constructor(props) {
-        console.log(">>call constructor: 1")
-        super(props);
-        this.state = {
-            isShowListUser: true
-        }
-    }
 
-    componentDidMount() {
-        console.log('>>call me component did mount')
-        setTimeout(()=> {
-            document.title = 'Khai dep trai'
-        },3000)
+// stateless vs stateful
+// class DisplayInfor extends React.Component {
+
+//     render() {
+//         console.log(">>>>call me render")
+//         // const  listUsers = this.props.listUsers;
+//         const { listUsers } = this.props;
+//         // console.log(listUsers)
+//         return (
+//             <div className='display-infor-container'>
+//                 {true &&
+//                     <>
+//                         {listUsers.map((user) => {
+//                             // console.log("check: ", user)
+//                             return (
+//                                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+//                                     <div style={{ color: 'purple', paddingTop: '10px' }} >My name's {user.name}</div>
+//                                     <div>My age's {user.age}</div>
+//                                     <div>
+//                                         <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
+//                                     </div>
+//                                     <hr></hr>
+//                                 </div>
+//                             )
+//                         })}
+//                     </>
+//                 }
+//             </div>
+//         )
+//     }
+// }
+
+const DisplayInfor = (props) => {
+    const { listUsers } = props;
+    const [isShowHideListUser, setShowHideListUser] = useState(true); 
+    const handleShowHideListUser = () =>{
+        setShowHideListUser(!isShowHideListUser);
     }
-    componentDidUpdate(prevProps,prevState,snapshot){
-        console.log('>>call me component did update', this.props, prevProps)
-        if(this.props.listUsers !== prevProps.listUsers){
-            if(this.props.listUsers.length === 5){
-                alert('You got 5 users')
-            }
-        }
-    }
-    handleShowHide = () => {
-        this.setState({
-            isShowListUser: !this.state.isShowListUser
-        })
-    }
-    render() {
-        console.log(">>>>call me render")
-        // const  listUsers = this.props.listUsers;
-        const { listUsers } = this.props;
-        // console.log(listUsers)
-        return (
-            <div className='display-infor-container'>
-                <img src={logo} />
-                <div>
-                    <span onClick={() => { this.handleShowHide() }}>
-                        {this.state.isShowListUser === true ? "Hide list users:" : "Show list users:"}
-                    </span>
-                </div>
-                {this.state.isShowListUser &&
-                    <>
-                        {listUsers.map((user) => {
-                            // console.log("check: ", user)
-                            return (
-                                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                                    <div style={{ color: 'purple', paddingTop: '10px' }} >My name's {user.name}</div>
-                                    <div>My age's {user.age}</div>
-                                    <div>
-                                        <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
-                                    </div>
-                                    <hr></hr>
-                                </div>
-                            )
-                        })}
-                    </>
-                }
+    return (
+        <div className='display-infor-container'>
+            <div>
+                <span onClick={()=> handleShowHideListUser()}>
+                    {isShowHideListUser === true ? "Hide list users" :"Show list users"}
+                </span>
             </div>
-        )
-    }
+            {isShowHideListUser &&
+                <>     
+                    {listUsers.map((user) => {
+                        // console.log("check: ", user)
+                        return (
+                            <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                                <div> 
+                                    <div>My name's {user.name}</div>
+                                    <div>My age's {user.age}</div>
+                                </div>
+                                <div>
+                                    <button onClick={() => props.handleDeleteUser(user.id)}>Delete</button>
+                                </div>
+                                <hr></hr>
+                            </div>
+                        )
+                    })}
+                </>
+            }
+        </div>
+    )
 }
+
+
 export default DisplayInfor;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,50 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUserInfor from './AddUserInfo';
 import DisplayInfor from './DisplayInfor';
-class MyComponent extends React.Component {
-    state = {
-        listUsers: [
-            { id: 1, name: "Ronaldo", age: "17" },
-            { id: 2, name: "Messi", age: "26" },
-            { id: 3, name: "Neymar", age: "24" }
-        ]
-    }
+// class MyComponent extends React.Component {
+//     state = {
+//         listUsers: [
+//             { id: 1, name: "Ronaldo", age: "17" },
+//             { id: 2, name: "Messi", age: "26" },
+//             { id: 3, name: "Neymar", age: "24" }
+//         ]
+//     }
+//     // DRY: Don't repeat yourself 
+
+//     handleAddNewUser = (userObj) => {
+//         this.setState({
+//             listUsers: [userObj, ...this.state.listUsers]
+//         })
+//     }
+
+//     handleDeleteUser = (userId) => {
+//         let listUsersClone = this.state.listUsers;
+//         listUsersClone = listUsersClone.filter(item => item.id !== userId)
+//         this.setState({
+//             listUsers: listUsersClone
+//         })
+//     }
+//     render() {
+//         const test = 'hocJSX';
+//         return (
+//             <>
+//                 {test}
+//                 <br />
+//                 <div className='a'>
+//                     <AddUserInfor
+//                         handleAddNewUser={this.handleAddNewUser}
+//                     />
+//                     <br></br>
+
+//                     <DisplayInfor
+//                         listUsers={this.state.listUsers}
+//                         handleDeleteUser={this.handleDeleteUser}
+//                     />
+//                 </div>
+//                 <div className='b'>
+//                 </div>
+//             </>
+//         );
+//     }
+// }
+
+
+
+const MyComponent = (props) => {
+    const [listUsers, setListUsers] = useState(
+        [{ id: 1, name: "Ronaldo", age: "17" },
+        { id: 2, name: "Messi", age: "26" },
+        { id: 3, name: "Neymar", age: "24" }
+        ])
     // DRY: Don't repeat yourself 
 
-    handleAddNewUser = (userObj) => {
-        this.setState({
-            listUsers: [userObj, ...this.state.listUsers]
-        })
+    const handleAddNewUser = (userObj) => {
+        setListUsers(userObj, ...listUsers);
+        // this.setState({
+        //     listUsers: [userObj, ...this.state.listUsers]
+        // })
     }
 
-    handleDeleteUser = (userId) => {
-        let listUsersClone = this.state.listUsers;
+    const handleDeleteUser = (userId) => {
+        let listUsersClone = listUsers;
         listUsersClone = listUsersClone.filter(item => item.id !== userId)
-        this.setState({
-            listUsers: listUsersClone
-        })
+        setListUsers(listUsersClone)
+        // this.setState({
+        //     listUsers: listUsersClone
+        // })
     }
-    render() {
-        const test = 'hocJSX';
-        return (
-            <>
-                {test}
-                <br />
-                <div classname='a'>
-                    <AddUserInfor
-                        handleAddNewUser={this.handleAddNewUser}
-                    />
-                    <br></br>
+    return (
+        <>
+            <br />
+            <div className='a'>
+                <AddUserInfor
+                    handleAddNewUser={handleAddNewUser}
+                />
+                <br /> 
 
-                    <DisplayInfor
-                        listUsers={this.state.listUsers}
-                        handleDeleteUser={this.handleDeleteUser}
-                    />
-                </div>
-                <div classname='b'>
-                </div>
-            </>
-        );
-    }
+                <DisplayInfor
+                    listUsers={listUsers}
+                    handleDeleteUser={handleDeleteUser}
+                />
+            </div>
+            <div className='b'>
+            </div>
+        </>
+    )
 }
 export default MyComponent;
