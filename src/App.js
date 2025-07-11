e161f06ba4726d62b1c58351f15dc84cb12c53b2
Richard Vu <vukhai09112005@gmail.com>
2025-07-11 23:18:42 +0700

    lamHeader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import { Outlet } from "react-router-dom"
 
 const App = () => {
-
   return (
-    <div className="App-container">
-      <Header/>
+    <div className="app-container">
+      <div className='header-container'>
+        <Header />
+      </div>
+      <div className='main-container'>
+        <div className='sidenav-container'>
+
+        </div>
+        
+        <div className='app-container'>
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 }
