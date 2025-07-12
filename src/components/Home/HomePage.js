85ebc5c2e8fde08bfd043613a0b437bdc0ec5897
Richard Vu <vukhai09112005@gmail.com>
2025-07-12 13:56:28 +0700

    lamHomePage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,25 @@
+import videoHomepage from '../../assets/video-homepage.mp4'
 const HomePage = (props) => {
     return(
         <div className="homepage-container">
-            home page component
+            <video autoPlay muted loop>
+                <source
+                    src={videoHomepage}
+                    type="video/mp4"
+               />
+            </video>
+            <div className='homepage-content'>
+                <div className='title-1'>
+                    There's a better way to practice Scrum
+                </div>
+                <div className='title-2'>
+                    You want to have more knowledge in Scrum and you don't have any source to learn.
+                    Go ahead here and you will gain more and more 
+                </div>
+                <div className='title-3'>
+                    <button>Get's started. It's free</button>
+                </div>
+            </div>
         </div>
     )
 }
