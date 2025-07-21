0bc315506777c8df0fa1c376fa9ddec19346b03a
Richard Vu <vukhai09112005@gmail.com>
2025-07-21 18:21:36 +0700

    lamView

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,32 +1,48 @@
 import { useEffect, useState } from 'react';
-import { getAllUsers } from '../../../services/apiServices';    
-import ModalCreateUser from "./ModalCreateUser";
+import { getAllUsers } from '../../../services/apiServices';
 import { FcPlus } from "react-icons/fc";
 import TableUser from "./TableUser";
+import ModalCreateUser from "./ModalCreateUser";
 import ModalUpdateUser from './ModalUpdateUser';
+import ModalViewUser from './ModalViewUser';
 
 const ManageUser = (props) => {
-
+    const [showModalViewUser, setshowModalViewUser] = useState(false);
     const [showModalCreateUser, setshowModalCreateUser] = useState(false);
-    const [showModalUpdateUser ,setshowModalUpdateUser] = useState(false);
+    const [showModalUpdateUser, setshowModalUpdateUser] = useState(false);
     const [dataUpdate, setDataUpdate] = useState({});
     const [listUsers, setListUsers] = useState([])
+    const [dataView, setDataView] = useState({});
+    
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchListUsers();
-    },[]);
+    }, []);
 
-    const fetchListUsers = async() => {
+    const fetchListUsers = async () => {
         let res = await getAllUsers()
         console.log(res);
-        if(res.EC === 0){
+        if (res.EC === 0) {
             setListUsers(res.DT)
         }
     }
-    const handleClickBtnUpdate=(user) => {
+
+    const handleClickBtnUpdate = (user) => {
         setshowModalUpdateUser(true);
         setDataUpdate(user);
     }
+    const resetUpdateData = () => {
+        setDataUpdate({});
+    }
+
+    const resetViewData = () => {
+        setDataView({});
+    };
+
+    const handleClickBtnView = (user) => {
+        setDataView(user);
+        setshowModalViewUser(true);
+    };
 
     return (
         <div className="manage-user-container">
@@ -41,9 +57,10 @@ const ManageUser = (props) => {
                     </button>
                 </div>
                 <div className="table-users-container">
-                    <TableUser 
-                    listUsers={listUsers}
-                    handleClickBtnUpdate={handleClickBtnUpdate}/>
+                    <TableUser
+                        listUsers={listUsers}
+                        handleClickBtnUpdate={handleClickBtnUpdate}
+                        handleClickBtnView={handleClickBtnView} />
                 </div>
                 <ModalCreateUser
                     show={showModalCreateUser}
@@ -54,6 +71,14 @@ const ManageUser = (props) => {
                     show={showModalUpdateUser}
                     setShow={setshowModalUpdateUser}
                     dataUpdate={dataUpdate}
+                    fetchListUsers={fetchListUsers}
+                    resetUpdateData={resetUpdateData}
+                />
+                <ModalViewUser
+                    show={showModalViewUser}
+                    setShow={setshowModalViewUser}
+                    dataView={dataView}
+                    resetViewData={resetViewData}
                 />
             </div>
         </div>
